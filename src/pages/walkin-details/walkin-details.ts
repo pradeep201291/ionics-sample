9747341b8c0414d14168b2ad1afe6336c15b5cef
PradeepKumar N <pradeep201291@gmail.com>
2018-06-02 02:38:52 +0530

    description page added

---
+++ src/pages/walkin-details/walkin-details.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'walkin-details',
+  templateUrl: 'walkin-details.html'
+})
+export class WalkinDetailsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
